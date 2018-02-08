4dcc4d26b608618134124ad152b72a0a573a709b
teabyii <teabyii@gmail.com>
2018-02-08 17:43:18 +0800

    chore: refine rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+  external: ['react', 'prop-types']
 }
